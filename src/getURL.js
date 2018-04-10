0901d111b73a215dcd8afacf8b7280192c614cbc
Muhammad Muhajir <muhajirframe@gmail.com>
2018-04-10 15:08:45 +0700

    Update getURL.js

--- src/getURL.js
+++ src/getURL.js
@@ -1,5 +1,5 @@
 export default function ({clientId, state, scope}) {
-  const current = encodeURIComponent(window.location.href)
+  const current = encodeURIComponent(`${window.location.protocol}//${window.location.host}/`)
   const base = 'https://www.linkedin.com/oauth/v2/authorization?response_type=code&'
   const fullScope = scope && scope.length ? `&scope=${encodeURIComponent(scope.join(' '))}` : ''
   return `${base}client_id=${clientId}&redirect_uri=${current}&state=${state}${fullScope}`
